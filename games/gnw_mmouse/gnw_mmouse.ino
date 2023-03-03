ce3d76cc0fc41a9011dee419b6218e9e2e868820
Yevgeniy <yevgeniy.olexandrenko@gmail.com>
2023-03-03 22:08:29 +0200

    Added pixelart drawing and testing code for gnw_mmouse

---
+++ games/gnw_mmouse/gnw_mmouse.ino
@@ -0,0 +1,54 @@
+#include "ArduboyGNW.h"
+#include "gnw_mmouse.hpp"
+
+Arduboy2Base arduboy;
+ArduboyGNW gnw(arduboy);
+
+enum State { None = 0x00, Demo, GameA, GameB, Loss };
+
+State detectState()
+{
+    // bool time   = gnw.segmentVisible(int(gnw_octopus_segment::time  ));
+    // bool game_a = gnw.segmentVisible(int(gnw_octopus_segment::game_a));
+    // bool game_b = gnw.segmentVisible(int(gnw_octopus_segment::game_b));
+    // bool death  = gnw.segmentVisible(int(gnw_octopus_segment::death ));
+    // bool man1   = gnw.segmentVisible(int(gnw_octopus_segment::man1  ));
+    // bool man2   = gnw.segmentVisible(int(gnw_octopus_segment::man2  ));
+    // bool man3   = gnw.segmentVisible(int(gnw_octopus_segment::man3  ));
+
+    // if (time && !game_a && !game_b) return State::Demo;
+    // if (!time && death && !man1 && !man2 && !man3) return State::Loss;
+    // if (!time && game_a) return State::GameA;
+    // if (!time && game_b) return State::GameB;
+    return State::None;
+}
+
+void setup()
+{
+    gnw.begin();
+    gnw.powerOn(
+        gnw_mmouse_controls,
+        gnw_mmouse_segments,
+        gnw_mmouse_graphics,
+        gnw_mmouse_gfxtiles,
+        gnw_mmouse_firmware
+    );
+}
+
+void loop()
+{
+    gnw.updateState(State::Demo);
+
+    gnw.setInput(GameAndWatch::ACL,    arduboy.pressed(UP_BUTTON));
+    gnw.setInput(GameAndWatch::TIME,   arduboy.pressed(DOWN_BUTTON));
+    gnw.setInput(GameAndWatch::GAME_A, arduboy.pressed(A_BUTTON));
+    gnw.setInput(GameAndWatch::GAME_B, arduboy.pressed(B_BUTTON));
+    gnw.setInput(GameAndWatch::RIGHT,  arduboy.pressed(RIGHT_BUTTON));
+    gnw.setInput(GameAndWatch::LEFT,   arduboy.pressed(LEFT_BUTTON));    
+
+    if (gnw.nextFrame())
+    {
+        gnw.drawLCD();
+        arduboy.display();
+    }
+}
